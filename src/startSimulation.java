3e90476c79ec70fff186083b40a77d1b43340ee3
sam <samuelapicella@hotmail.com>
2017-07-05 16:11:07 +0200

    Finale

--- src/startSimulation.java
+++ src/startSimulation.java
@@ -5,8 +5,9 @@ import java.util.concurrent.Executors;
 class startSimulation {
     ArrayList<ArrayList<Integer>> start() throws InterruptedException, NoSuchIndividuoException {
 
-        Popolazione popolazione = new Popolazione(1000,1000,1000,1000, 15, 20, 3, 20, 7);
-        int iterazioni = 30;
+        Popolazione popolazione = new Popolazione(1000,1000,1000,1000,
+                15, 20, 3, 70, 8);
+        int iterazioni = 100;
 
         ArrayList<ArrayList<Integer>> data = new ArrayList<>();
         ArrayList<Integer> nMor = new ArrayList<>(), nPru = new ArrayList<>(), nAvv = new ArrayList<>(), nSpr = new ArrayList<>();
@@ -16,13 +17,10 @@ class startSimulation {
 
         boolean flag = true;
 
-        //int a = popolazione.getA(); int b = popolazione.getB(); int c = popolazione.getC();
-
         for(int i = 0; i < iterazioni; i++){
 
             for(int j = 0; j < popolazione.getMaschi().size(); j++)
                 popolazione.getMaschi().get(j).setEta(1);
-
 
             for(int k = 0; k < popolazione.getFemmine().size(); k++)
                 popolazione.getFemmine().get(k).setEta(1);
@@ -72,7 +70,7 @@ class startSimulation {
 
                 executorService.submit(new Accoppiamento(maschio,femmina,popolazione));
                 long estimatedTime = System.currentTimeMillis() - startTime;
-                if(estimatedTime>3000) flag = false;
+                if(estimatedTime>100) flag = false;
             }
 
             executorService.shutdownNow();
@@ -83,23 +81,7 @@ class startSimulation {
             nAvv.add(popolazione.getNumberAvventurieri());
             nPru.add(popolazione.getNumberPrudenti());
             nSpr.add(popolazione.getNumberSpregiudicate());
-            /*
-            double ratioM_Male = ((double)pop.getNumberMorigerati()/pop.getMaschi().size());
-            double ratioP_Female = ((double) pop.getNumberPrudenti()/pop.getFemmine().size());
 
-            double Va_M = ratioP_Female*(a-(b/2.0)-c)+(1-ratioP_Female)*(a-(b/2.0));
-            double Va_A = a*(1-ratioP_Female);
-
-            double Va_P = ratioM_Male*(a-(b/2.0)-c);
-            double Va_S = ratioM_Male*(a-(b/2.0))+(1-ratioM_Male)*(a-(b));
-
-
-
-            System.out.println("Valori attesi:");
-            System.out.println("VA_m: "+Va_M);
-            System.out.println("VA_a: "+Va_A);
-            System.out.println("VA_p: "+Va_P);
-            System.out.println("VA_s: "+Va_S);*/
             System.out.println();
 
             flag = true;
